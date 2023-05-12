7220d3d303acb57fd2c456bd797c406bd29e29a8
Maillardjay <jay_10@hotmail.fr>
2023-05-12 02:12:33 +0200

    ajout de d'un map, d'un input et d'un filter sur le composant home + creation d'un composant housecard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,17 @@ import NavBar from "./pages/NavBar";
 import Home from "./pages/Home";
 import DescriptionPage from "./pages/DescriptionPage";
 import BasketPage from "./pages/BasketPage";
-import Footer from "./pages/Footer";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Description" element={<DescriptionPage />} />
           <Route path="/BasketPage" element={<BasketPage />} />
         </Routes>
-        <Footer />
+        <NavBar />
       </div>
     </Router>
   );
---
+++ frontend/src/components/HouseCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function HouseCard({ haunted }) {
+  return (
+    <>
+      <div className="card-header">
+        <div className="card-img">
+          <img src={haunted.image} alt="haunted-img" />
+        </div>
+      </div>
+      <div className="card-Body">
+        <h2 className="card-title">{haunted.title}</h2>
+        <p className="card-description">{haunted.description}</p>
+      </div>
+    </>
+  );
+}
+
+export default HouseCard;
