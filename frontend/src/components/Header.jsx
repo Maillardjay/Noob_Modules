3dcbd233685c0be9380c068ef920f91961d6d04d
Laure Graves <graveslaure@gmail.com>
2023-05-12 11:10:05 +0200

    modification logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import "../Styles/Header.scss";
 
-// function Header() {
-//   return (
-//     <div>
-//       <header className="header-container">
-//         <div className="main-title">
-//           <div className="logo">
-//           <img src="https://media.discordapp.net/attachments/1105773595063488605/1106323132467388456/maison_hantee.jpg" alt="Logo maison hantée"/>
-//           </div>
-//           <h2 className="header-title">LES VACANCES DE L'HORREUR</h2>
-//         </div>
-//       </header>
-//     </div>
-
-//   );
-// }
-
 function Header() {
   return (
     <div className="Header">
       <img
-        src="https://media.discordapp.net/attachments/1105773595063488605/1106323132467388456/maison_hantee.jpg"
-        alt="Logo maison hantée"
+        src="https://media.discordapp.net/attachments/1105773595063488605/1106484097406222346/concept-conception-logo-fantome-silhouette-simple-isole-fond-blanc_10250-5358.png?width=609&height=609"
+        alt="Logo fantome"
       />
       <h1>Les vacances de l'horreur</h1>
     </div>
